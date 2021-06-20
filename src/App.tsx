86e656e08aee5cf6661967f669ddc84a3107f113
Aimen <aimenhamed@live.com.au>
2021-06-20 22:42:04 +1000

    Remove from cart feature

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,20 @@ const App = () => {
     });
   };
 
-  const handleRemoveFromCart = () => null;
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev => (
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) {
+            return ack;
+          }
+          return [...ack, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[])
+    ));
+  };
 
   if (isLoading) {
     return <LinearProgress />
