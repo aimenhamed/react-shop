4b2dd7a234960cfd81e32d2fe80aeb262655b69c
Aimen <aimenhamed@live.com.au>
2021-06-21 21:23:15 +1000

    style changes

--- src/Cart/Cart.styles.ts
+++ src/Cart/Cart.styles.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.aside`
     font-family: Arial, Helvetica, sans-serif;
-    width: 500px
+    width: 500px;
     padding: 20px;
 `;
--- src/CartItem/CartItem.styles.ts
+++ src/CartItem/CartItem.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
     display: flex;
-    justify-contnet: space-between;
+    justify-content: space-between;
     font-family: Arial, Helvetica, sans-serif;
     border-bottom: 1px solid lightblue;
     padding-bottom: 20px;
